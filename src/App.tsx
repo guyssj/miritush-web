5a047e68e54eb8d58ff916400242c1bbeb9e8988
Guy Mac Mini <Guyssj@gmail.com>
2021-08-30 19:42:49 +0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import Navbar from './components/NavBar/navbar';
+import Slider from "react-slick";
 
 
 function App() {
-  return (   
-    <Navbar />
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </React.Fragment>
 
   );
 }
--- src/components/NavBar/navbar.tsx
+++ src/components/NavBar/navbar.tsx
@@ -1,10 +1,33 @@
 import React, { useEffect } from 'react';
 import './navbar.css';
 import Logo from '../../logo.svg';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Button, createStyles, makeStyles, Theme } from '@material-ui/core';
+import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    toolbar: {
+      color:'white',
+      backgroundColor:'#000000'
+    },
+  }),
+);
 
 const Navbar = () => {
+  const preventDefault = (event: React.SyntheticEvent) => event.preventDefault();
   const [scrolled, setScrolled] = React.useState(false);
-
+  const classes = useStyles();
   const handleScroll = () => {
     const offset = window.scrollY;
     if (offset > 200) {
@@ -23,24 +46,37 @@ const Navbar = () => {
     x.push('scrolled');
   }
   return (
-    <header className={x.join(" ")} dir="rtl">
-      <nav className="navigation">
-        <ul>
-          <li className="nav-item" ><a className="nav-link" href="#post1">דף הבית</a></li>
-          <li className="nav-item"><a  className="nav-link"href="#post2">Home</a></li>
-        </ul>
-        <div className="logo">
-          <img src={Logo} alt="Logo" title="Logo" />
-        </div>
-        <ul>
-        <li className="nav-item">
-          <a className="nav-link" href="#post1">Home</a>
-          </li>
-          <li className="nav-item"><a className="nav-link" href="#post2">Home</a></li>
-        </ul>
-      </nav>
+    <AppBar position="static">
+  <Toolbar className={classes.toolbar}>
+    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <MenuIcon />
+    </IconButton>
+    <Typography variant="h6">
+    <Link href="#" onClick={preventDefault} color="inherit">
+    {'color="inherit"'}
+  </Link>
+    </Typography>
+  </Toolbar>
+</AppBar>
+    // <header className={x.join(" ")} dir="rtl">
+    //   <nav className="navigation transparent-navbar sticky">
+    //     <ul className="navBarRight">
+    //       <li className="nav-item" ><a className="nav-link" href="#post1">דף הבית</a></li>
+    //       <li className="nav-item"><a  className="nav-link"href="#post2">שינוי תור</a></li>
+    //     </ul>
+    //     <div className="logo">
+    //       <img src={Logo} alt="Logo" title="Logo" />
+    //     </div>
+    //     <ul className="navBarLeft">
+    //     <li className="nav-item">
+    //       <a className="nav-link" href="#post1">גלריה</a>
+    //       </li>
+    //       <li className="nav-item"><a className="nav-link" href="#post2">מועדון</a></li>
+    //     </ul>
+    //   </nav>
 
-    </header>
+    // </header>
+    
   )
 };
 
